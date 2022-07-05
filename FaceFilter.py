bfcc9903e35a2796ebf9ad0f6509e6a300a7f7c7
AbhirathSP <abhirathsp2@gmail.com>
2022-07-05 17:31:31 +0530

    A

--- FaceFilter.py
+++ FaceFilter.py
@@ -1,2 +1,10 @@
-from cv2 import cv2
+import cv2
 import cvzone
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    cv2.imshow('Snap Dude', frame)
+    if cv2.waitKey(10) == ord('q'):
+        break
